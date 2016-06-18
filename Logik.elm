8abb9ab1d0a080edbdf68ed3bb6978e193f5d6f5
Paul Scherer <scherer@cs.uni-bonn.de>
2016-06-18 20:58:18 +0200

    Anlegen von Logik.elm, Operationen.elm und Sudoku.elm mit leerem Inhalt

---
+++ Logik.elm
@@ -0,0 +1,1 @@
+module Logik exposing (..)
---
+++ Sudoku.elm
@@ -0,0 +1,13 @@
+module Sukoku exposing (..)
+
+import Logik exposing (..)
+import Operationen exposing (..)
+
+main =
+    programm
+    {
+    init = 
+    , view = 
+    , update = 
+    , subscriptions = subscriptions
+    }
