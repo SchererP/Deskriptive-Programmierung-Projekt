42a06fc8234f1b128e0827becaf2e167a659a4ab
SchererP <SchererP@users.noreply.github.com>
2016-07-04 12:16:55 +0200

    Deklaration der Logik-Funktionen
    Funktionen: generiereFeld, überprüfeFeld, möglicheZiffern

--- Logik.elm
+++ Logik.elm
@@ -1,1 +1,16 @@
 module Logik exposing (..)
+
+import Operationen exposing (..)
+
+generiereFeld : int -> Sudokufeld
+generiereFeld = 
+-- Generiert ein eindeutig-lösbares Feld und entfernt ableitbare Einträge
+-- Der int-Wert gibt den Schwierigkeitsgrad an
+
+ueberpruefeFeld : Sudokufeld -> Maybe Bool
+ueberpruefeFeld = 
+-- Kontrolliert, ob das Feld vollständig gefüllt und korrekt belegt wurde. Für nicht-vollständige Felder wird Nothing zurückgegeben und sonst Bool
+
+moeglicheZiffern : Sudokufeld -> Pos -> List int
+moeglicheZiffern = 
+-- Liefert eine Liste mit möglichen Einträgen
