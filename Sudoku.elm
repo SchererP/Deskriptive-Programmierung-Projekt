8717e2d8d736eda76781b07feadebea5fb63b3d4
SchererP <SchererP@users.noreply.github.com>
2016-07-04 11:23:03 +0200

    Hinzufügen des Models zu Sudoku.elm
    Entwicklung des Models für die Sudoku-Implementierung; Felder: spielfeld, modus, markiert

--- Sudoku.elm
+++ Sudoku.elm
@@ -11,3 +11,8 @@ main =
     , update = 
     , subscriptions = subscriptions
     }
+    
+type alias model = { spielfeld : Sudokufeld, modus : Bool, markiert : Pos }
+-- spielfeld speichert das Array mit Typ Maybe int, Bool; Maybe int legt den Inhalt fest und Bool speichert, ob es sich um einen Startwert handelt
+-- modus legt fest, ob die möglichen Einträge angezeigt werden oder nicht
+-- markiert speichert die Koordinaten des aktuell ausgewählten Feldes
