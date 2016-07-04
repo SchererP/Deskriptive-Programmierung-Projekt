ea542692c20dda1d98461b9c37f512f0939d447d
SchererP <SchererP@users.noreply.github.com>
2016-07-04 11:55:11 +0200

    Deklaration der Operationen
    Hinzufügen der Operationen, die für Logik und Sudoku gebraucht werden, und Implementierung von emptyField.
    Parameter müssen noch gesetzt werden.

--- Operationen.elm
+++ Operationen.elm
@@ -2,3 +2,28 @@ module Operationen exposing (..)
 
 type alias Pos = (int, int)
 type alias Sudokufeld = Array (Array (Maybe int, Bool))
+
+getField : Sudokufeld -> Pos -> Maybe int
+getField = 
+
+setField : Sudokufeld -> Pos -> int -> Sudokufeld
+setField = 
+
+getColumn : Sudokufeld -> int -> List (Maybe int)
+getColumn = 
+
+getRow : Sudokufeld -> int -> List (Maybe int)
+getRow = 
+
+getSubsquare : Sudokufeld -> int -> List (Maybe int)
+getSubsquare = 
+
+setProtected : Sudokufeld -> Pos -> Sudokufeld
+setProtected = 
+
+emptyField : Sudokufeld
+emptyField = Array.repeat 9 (Array.repeat 9 (Nothing, False))
+-- Erstellt ein leeres Feld, bei dem kein Schreibschutz aktiviert ist
+
+toHMTL : Sudokufeld -> HTML
+toHTML = 
