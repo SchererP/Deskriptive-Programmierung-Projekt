3254021a157d9922de6023dbf87be194d7a055da
MarsM0nd <MarsM0nd@users.noreply.github.com>
2016-07-14 12:49:04 +0200

    Implementation der meisten Funktionen
    Die implementierten Funktionen liefern kein compilierungsfehler und sollten das richtige Ergebnis liefern wenn es eine valide Eingabe war. Elm-format wurde benutzt.

--- Operationen.elm
+++ Operationen.elm
@@ -1,28 +1,134 @@
 module Operationen exposing (..)
+import Array exposing (..)
+import List
 
-type alias Pos = (int, int)
-type alias Sudokufeld = Array (Array (Maybe int, Bool))
 
-getField : Sudokufeld -> Pos -> Maybe int
-getField = 
+type alias Pos =
+    ( Int, Int )
 
-setField : Sudokufeld -> Pos -> int -> Sudokufeld
-setField = 
 
-getColumn : Sudokufeld -> int -> List (Maybe int)
-getColumn = 
+type alias Sudokufeld =
+    Array (Array ( Maybe Int, Bool ))
 
-getRow : Sudokufeld -> int -> List (Maybe int)
-getRow = 
 
-getSubsquare : Sudokufeld -> int -> List (Maybe int)
-getSubsquare = 
+getField : Sudokufeld -> Pos -> Maybe Int
+getField feld pos =
+    case get (fst pos) feld of
+        Nothing ->
+            Nothing
+
+        Just row ->
+            case get (snd pos) row of
+                Nothing ->
+                    Nothing
+
+                Just feld ->
+                    fst feld
+
+
+setField : Sudokufeld -> Pos -> Int -> Sudokufeld
+setField feld pos zahl =
+    let
+        modzahl =
+            if zahl == 0 then
+                Nothing
+            else
+                Just zahl
+    in
+        case isProtected feld pos of
+            False ->
+                let
+                    row =
+                        case get (fst pos) feld of
+                            Just row' ->
+                                set (snd pos) ( modzahl, False ) row'
+
+                            Nothing ->
+                                empty
+                in
+                    set (fst pos) row feld
+
+            True ->
+                feld
+
+
+getColumn : Sudokufeld -> Int -> List (Maybe Int)
+getColumn feld spalte =
+    let
+        specialGet =
+            \x y ->
+                case get x y of
+                    Nothing ->
+                        ( Just 0, False )
+
+                    Just smth ->
+                        smth
+    in
+        List.map fst (toList (map (specialGet spalte) feld))
+
+
+getRow : Sudokufeld -> Int -> List (Maybe Int)
+getRow feld zeile =
+    let
+        zeilenbool =
+            case get zeile feld of
+                Nothing ->
+                    []
+
+                Just zeilenbool' ->
+                    toList zeilenbool'
+    in
+        List.map fst zeilenbool
+
+
+getSubsquare : Sudokufeld -> Int -> List (Maybe Int)
+getSubsquare feld unterfeld =
+    let
+        row =
+            List.map ((*) ((unterfeld - 1) // 3 + 1)) [1..3]
+
+        column =
+            List.map ((*) ((unterfeld - 1) % 3 + 1)) [1..3]
+    in
+        let
+            poses =
+                List.map2 (,) row column
+        in
+            List.map (getField feld) poses
+
 
 setProtected : Sudokufeld -> Pos -> Sudokufeld
-setProtected = 
+setProtected feld pos =
+    let
+        row =
+            case get (fst pos) feld of
+                Just row' ->
+                    set (snd pos) ( getField feld pos, True ) row'
+
+                Nothing ->
+                    empty
+    in
+        set (fst pos) row feld
+
+
+isProtected : Sudokufeld -> Pos -> Bool
+isProtected feld pos =
+    case get (fst pos) feld of
+        Nothing ->
+            False
+
+        Just row ->
+            case get (snd pos) row of
+                Nothing ->
+                    False
+
+                Just intbool ->
+                    snd intbool
+
 
 emptyField : Sudokufeld
-emptyField = Array.repeat 9 (Array.repeat 9 (Nothing, False))
+emptyField =
+    Array.repeat 9 (Array.repeat 9 ( Nothing, False ))
 -- Erstellt ein leeres Feld, bei dem kein Schreibschutz aktiviert ist
 
 toHMTL : Sudokufeld -> HTML
