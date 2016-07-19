f367ed5b0390646c3ad5883cd57c8fa8a2a70f60
Jonas <barteld@cs.uni-bonn.de>
2016-07-19 16:48:47 +0200

    Verschiedene Funktionen
    Umbenennung von Tipp zu Autofüllen, Zufallsauswahl von  Spielfeldern,
    Markierung von schreibgeschützten Feldern, bessere Darstellung leerer
    Felder.

--- Sudoku.elm
+++ Sudoku.elm
@@ -12,7 +12,7 @@ import Time exposing (Time, second)
 
 import Html.Events exposing (onClick, onCheck)
 import List exposing (map)
-
+import Random exposing (int, generate)
 
 type Msg
     = Moduswechsel Bool
@@ -20,10 +20,12 @@ type Msg
     | Zifferneingabe Int
     | Abschluss
     | Tipp
-
+    | NeuesFeld
+    | WaehleFeld Int
+    | SetzeAutofill (Maybe (Pos, Int))
 
 init =
-    ( { spielfeld = generiereFeld 1, modus = True, markiert = ( 1, 1 ), abschluss = Nothing, tippMoeglich = Nothing }, Cmd.none )
+    ( { spielfeld = generiereFeld 1, modus = True, markiert = ( 1, 1 ), abschluss = Nothing, autofill = Nothing }, Cmd.none )
 
 
 main =
@@ -36,7 +38,7 @@ main =
 
 
 type alias Model =
-    { spielfeld : Sudokufeld, modus : Bool, markiert : Pos, abschluss : Maybe Bool }
+    { spielfeld : Sudokufeld, modus : Bool, markiert : Pos, abschluss : Maybe Bool, autofill : Maybe Bool }
 
 
 
@@ -47,11 +49,11 @@ type alias Model =
 
 view model =
     div []
-        [ header [] [ text "Sudoku" ]
+        [ --header [] [ text "Sudoku" ]
           --Anpassung des Headers im Browser
-        , h1 [] [ text "Sudokuspiel" ]
+         h1 [] [ text "Sudokuspiel" ]
           --Titel des Spiels
-        , table [] [ tr [] [ td [] [ text "Modus:" ] ], tr [] [ td [] [ select [] [ option [ onClick (Moduswechsel True) ] [ text "Mit Hilfestellung" ], option [ onClick (Moduswechsel False) ] [ text "Ohne Hilfestellung" ] ] ] ] ]
+        , table [] [ tr [] [ td [] [ text "Modus:" ] ], tr [] [ td [] [ select [] [ option [ onClick (Moduswechsel True) ] [ text "Mit Hilfestellung" ], option [ onClick (Moduswechsel False) ] [ text "Ohne Hilfestellung" ] ] ], td [] [button [ onClick NeuesFeld ] [ text "Neues Feld" ]] ] ]
           --Menü zur Modusauswahl
         , br [] [text "Warum verschwindet dieser Text?"]
         , druckeSpielfeld model.spielfeld model.markiert
@@ -62,7 +64,7 @@ view model =
         , br [] [text "Warum verschwindet dieser Text?"]
         , button [ onClick Abschluss ] [ text "Abschlusskontrolle" ]
           --Anstoß der Kontrollroutine
-        , button [ onClick Tipp ] [ text "Tipp" ]
+        , button [ onClick Tipp ] [ text "Autofüllen" ]
         , br [] [text "Warum verschwindet dieser Text?"]
         , br [] [text "Warum verschwindet dieser Text?"] --Dummyelement, um einen Zeilenumbruch zu realisieren
         , case model.abschluss of
@@ -75,12 +77,20 @@ view model =
 
             Just False ->
                 text "Das Feld war bei der letzten Kontrolle leider nicht korrekt gefüllt."
+        , br [] [text "Warum verschwindet dieser Text?"]
+        , case model.autofill of
+            Nothing ->
+              text "Autofill könnte möglich sein."
+            Just True ->
+              text "Es wurde ein Feld automatisch gefüllt."
+            Just False ->
+              text "Es wurde kein automatisch füllbares Feld gefunden."
         , h2 [] [ text "Programmerklärungen:" ]
           --Hinweistexte für die auszuwählenden Menüs
         , h3 [] [ text "Modus" ]
         , p [] [ text "Das Spiel bietet zwei Modi an: mit Hilfestellung oder ohne. Mit Hilfestellung werden alle für das ausgewählte Feld ungültigen Ziffern ausgeblendet und ohne Hilfestellung alle Ziffern immer zur Auswahl gestellt." ]
-        , h3 [] [text "Tipp" ]
-        , p [] [ text "Mit 'Tipp' wird ein eindeutig lösbares Feld angezeigt, sofern der aktuelle Zustand des Feldes nicht wegen falscher Belegung(en) unlösbar ist. Wird kein Eintrag verändert, so ist das Spielfeld von der aktuellen Konfiguration ausgehend nicht lösbar." ]
+        , h3 [] [text "Autofüllen" ]
+        , p [] [ text "Mit 'Autofüllen' wird ein eindeutig lösbares Feld angezeigt, sofern der aktuelle Zustand des Feldes nicht wegen falscher Belegung(en) unlösbar ist. Wird kein Eintrag verändert, so ist kein direkt eindeutiges Feld vorhanden oder das Spielfeld ist nicht mehr lösbar." ]
         , br [] [text "Warum verschwindet dieser Text?"]
         , br [] [text "Warum verschwindet dieser Text?"]
         , footer [] [ text "Ein Projekt von Jonas Barteldrees und Paul Scherer im Rahmen der Vorlesung Deskriptive Programmierung im Sommersemester 2016"]
@@ -113,17 +123,19 @@ druckeSpielfeld feld markiert =
     let
         toButton =
             \pos -> td [ style [if pos == markiert then ("backgroundColor", "blue") else ("backgroundColor", "white"), ("border", "50"), ("borderColor", "black")]] [ button [ onClick (Feldauswahl pos)] [ druckeZahl feld pos ]]
-    in
-        let
-            toButtonRow =
-                \x -> tr [] (map ((curry toButton) x) [1..9])
+        toButtonRow =
+            \x -> tr [] (map ((curry toButton) x) [1..9])
         in
             table [] (map toButtonRow [1..9])
 
 druckeZahl : Sudokufeld -> Pos -> Html Msg
 druckeZahl feld pos = case getField feld pos of
-                            Just i -> text (toString i)
-                            Nothing -> span [] [text "   "]
+                            Just i -> if (isProtected feld pos) then strong [] [text (toString i)] else text (toString i)
+                            Nothing -> text "_"
+
+zufaelligesFeld : Int->Msg
+zufaelligesFeld feldint=
+  WaehleFeld feldint
 
 update msg model =
     case msg of
@@ -131,7 +143,7 @@ update msg model =
             ( { model | markiert = pos }, Cmd.none )
 
         Zifferneingabe i ->
-            ( { model | spielfeld = setField model.spielfeld model.markiert i }, Cmd.none )
+            ( { model | spielfeld = setField model.spielfeld model.markiert i, autofill = if (isProtected model.spielfeld model.markiert) then model.autofill else Nothing }, Cmd.none )
 
         Abschluss ->
             ( { model | abschluss = ueberpruefeFeld model.spielfeld }, Cmd.none )
@@ -139,10 +151,19 @@ update msg model =
         Moduswechsel b ->
             ( { model | modus = b }, Cmd.none )
         Tipp ->
-            ({model | spielfeld = case berechneTipp model.spielfeld (1, 1) of
-                                        Just (pos, i) -> setField model.spielfeld pos i
-                                        Nothing -> model.spielfeld
-                                  }, Cmd.none)
+            (model,generate (\_->SetzeAutofill (berechneTipp model.spielfeld (1, 1))) (int 0 0))
+        SetzeAutofill tipp ->
+            let
+              newfield = case tipp of
+                          Just (pos, i) -> setField model.spielfeld pos i
+                          Nothing -> model.spielfeld
+          in
+            ({model | spielfeld = newfield, autofill = if tipp == Nothing then Just False else Just True}, Cmd.none)
+        NeuesFeld ->
+          ( model, generate zufaelligesFeld (int 1 20))
+
+        WaehleFeld i ->
+          ({model|spielfeld = generiereFeld i}, Cmd.none)
 
 
 subscriptions model =
